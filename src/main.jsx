61a51f597cee39a06f4e75010ab043d9b928e028
Leah Obot <leahobot@gmail.com>
2023-04-10 14:06:57 +0100

    Publishing project to production

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ContextProvider } from "./context/ContextProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<ContextProvider>
 				<App />
 			</ContextProvider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 );
